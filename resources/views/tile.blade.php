3dd4afbe06d7829a28ef2b10f59f33c1a05d2dcc
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-12-31 00:07:23 -0300

    Fix error MultipleRootElementsDetectedException

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -3,26 +3,26 @@
     <div class="text-sm font-bold text-center">{!! $articleContent !!}</div>
 
     <img src="{!! $articleQrCode !!}" alt="QR Code" id="qrcode" />
+    <style>
+        img{
+            padding:20px;
+        }
+        p{
+            display: none;
+        }
+        p:first-child{
+            display: block;
+        }
+        hr{
+            display: none;
+        }
+        #qrcode{
+            position: absolute;
+            background-color: white;
+            bottom: 5px;
+            left: 5px;
+            width: 15%;
+            padding:0px;
+        }
+    </style>
 </x-dashboard-tile>
-<style>
-    img{
-        padding:20px;
-    }
-    p{
-        display: none;
-    }
-    p:first-child{
-        display: block;
-    }
-    hr{
-        display: none;
-    }
-    #qrcode{
-        position: absolute;
-        background-color: white;
-        bottom: 5px;
-        left: 5px;
-        width: 15%;
-        padding:0px;
-    }
-</style>
